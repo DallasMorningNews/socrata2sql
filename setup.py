87c85f117dcc65db441a1d26d6f9194a17718ed1
Andrew Chavez <adchavez@gmail.com>
2019-02-08 10:01:00 -0600

    Add README to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+from os import path
 from setuptools import setup
 
 from socrata2sql import __version__
 
 
+readme_path = path.join(path.abspath(path.dirname(__file__)), 'README.md')
+with open(readme_path, encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='socrata2sql',
     version=__version__,
     description='SQL loader for Socrata data sets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/DallasMorningNews/socrata2sql',
     author='Andrew Chavez / The Dallas Morning News',
     author_email='newsapps@dallasnews.com',
